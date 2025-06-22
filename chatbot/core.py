dab45c478d4c3b711bf310436b74b308305d6439
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-22 17:41:11 -0400

    Add ChatCore class with initial mood based responses

---
+++ chatbot/core.py
@@ -0,0 +1,8 @@
+class ChatCore:
+    
+    def __init__(self):
+        self.responses = {
+            "sad" : "I'm really sorry to hear that.", 
+            "anxious": "Let's take a deep breathe.", 
+            "happy": "That's great to hear"
+        }
