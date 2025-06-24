5ea04b60510d3d05045cb72c17c5d51ad698c335
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-24 19:00:12 -0400

    Add docstrings and comments to ChatCore class
    Added detailed docstrings and inline comments to the ChatCore class and its methods in chatbot/core.py.

--- chatbot/core.py
+++ chatbot/core.py
@@ -1,21 +1,39 @@
+"""
+This module have the core logic for handling user interaction, mood detection, internet checks, and integrating 
+AI responses.
+"""
+
 from .prompts import load_supportive_messages
 import random
 from gemini_client import gen_gemini_response
 import socket
 
+# Main class responsible for chatbot behavior.
 class ChatCore:
     
     def __init__(self):
+        # predefined mood-based responses.
         self.responses = {
             "sad" : "I'm really sorry to hear that.", 
             "anxious": "Let's take a deep breathe.", 
             "happy": "That's great to hear."
         }
         
+        # Stores chat history and loads offline supportive messages.
         self.convo_history = []
         self.supportive_messages = load_supportive_messages()
     
     def detect_mood(self, user_input):
+        """
+        Analyzes the user input to detect emotional tone.
+
+        Args:
+            user_input (str): The text entered by the user.
+
+        Returns:
+            str: The detected mood keyword or "neutral" if none is matched.
+        """
+        
         user_input = user_input.lower()
         
         for mood in self.responses:
@@ -24,6 +42,16 @@ class ChatCore:
         return "neutral"
     
     def give_a_response(self, user_input):
+        """
+        Returns a response based on detected mood or randomly selects a supportive message.
+
+        Args:
+            user_input (str): The user's message.
+
+        Returns:
+            str: A response either based on mood or a fallback supportive message.
+        """
+        
         mood = self.detect_mood(user_input)
         
         self.convo_history.append({"role": "user", "content": user_input})
@@ -34,6 +62,13 @@ class ChatCore:
             return random.choice(self.supportive_messages)
         
     def is_there_internet(self):
+        """
+        Checks for internet connectivity by attempting to reach a known ip address.
+
+        Returns:
+            bool: if the internet is reachable it returns True, if not it returns False.
+        """
+        
         target_ip = '8.8.8.8'
         target_port = 53
         
@@ -48,6 +83,16 @@ class ChatCore:
             return False
         
     def chat(self, user_input):
+        """
+        Main method to handle user message. It appends the message to the chat history only if online.
+
+        Args:
+            user_input (str): The user's message.
+
+        Returns:
+            str: AI-generated or supportive fallback responses.
+        """
+        
         if user_input == "":
             return "What? is wrong? Let me help."
         
