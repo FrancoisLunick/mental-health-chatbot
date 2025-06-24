8760284df95ce06ea75a74da6d0e6c1cba84a2d4
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-23 20:38:18 -0400

    Add function to log user and bot chat
    Introduced log_user_and_bot_chat to append user and bot messages to logs/chat_log.txt for chat history tracking.

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -7,3 +7,10 @@ def clean_text(text):
 clean_user_text = clean_text("testing text")
 
 print(clean_user_text)
+
+def log_user_and_bot_chat(user_input, bot_input):
+    with open("logs/chat_log.txt", "a") as file:
+        file.write(f"User: {user_input}\n")
+        file.write(f"Bot: {bot_input}\n\n")
+    
+    file.close
