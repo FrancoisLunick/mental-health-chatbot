98b9e60fe499edaa6318397218f4ed5ae85a4d08
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-24 15:17:53 -0400

    Fix input handling and response in main loop
    Moves initial user input outside the loop, corrects string comparison to use '==', and fixes function call to 'give_a_response()' for proper response output.

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ chat_core = ChatCore()
 
 print("Hey there welcome. \n\n")
 
+user_input = input("Tell me how you are feeling today. \n")
+
 while True:
-    user_input = input("Tell me how you are feeling today. \n")
+    user_input = input("You: ")
     
-    if user_input is "exit" or user_input is "quit":
+    if user_input == "exit" or user_input == "quit":
         break
     else:
         chat_core.chat(user_input)
-        print(chat_core.give_a_response)
+        print(chat_core.give_a_response())
 
 print("Have a great day. I'll be here if you ever need me again. Goodbye.")
