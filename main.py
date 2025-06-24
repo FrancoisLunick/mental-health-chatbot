1368ccd9960729830afa72ef2de7e9ed73f4ee27
Lunick Francois <LunickFrancois22@gmail.com>
2025-06-24 15:12:43 -0400

    Add chat loop and exit condition to main.py
    Replaces single input with a loop to allow multiple user interactions. Adds exit conditions for 'exit' and 'quit' commands, and prints a farewell message upon exit.

--- main.py
+++ main.py
@@ -4,4 +4,13 @@ chat_core = ChatCore()
 
 print("Hey there welcome. \n\n")
 
-user_input = input("Tell me how you are feeling today. \n")
+while True:
+    user_input = input("Tell me how you are feeling today. \n")
+    
+    if user_input is "exit" or user_input is "quit":
+        break
+    else:
+        chat_core.chat(user_input)
+        print(chat_core.give_a_response)
+
+print("Have a great day. I'll be here if you ever need me again. Goodbye.")
